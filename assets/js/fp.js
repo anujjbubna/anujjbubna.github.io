4128948e4bafe913d0d5cc9a01856d8b2b289694
anujjbubna <anujjbubna39@gmail.com>
2020-05-18 12:39:25 -0400

    Website update.

--- assets/js/fp.js
+++ assets/js/fp.js
@@ -2,8 +2,9 @@ new fullpage('#fullpage', {
 	//options here
 	autoScrolling:true,
     fadingEffect: true,
-	scrollHorizontally: true
+	// scrollHorizontally: true
 });
 
 //methods
 fullpage_api.setAllowScrolling(true);
+fullpage_api.setautoScrolling(true);
